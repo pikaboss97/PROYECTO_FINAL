75c50597b32b849e458d48bc92f2a76b4f8a8111
Diego <diego.vega097@gmail.com>
2019-07-17 17:37:36 -0500

    Rutas del servidor definidas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const home = require('../controllers/home');
+const image = require('../controllers/image');
+
 module.exports = app => {
 
-    app.get('/', (req,res) =>{
-        res.send('idex page');
-    })
+    router.get('/', home.index);
+    router.get('/images/:image_id', image.index);
+    router.post('/images', image.create);
+    router.post('/images/:image_id/like', image.like);
+    router.post('/images/:image_id/cocomment', image.comment);
+    router.delete('/images/:image_id', image.remove);
+
+    app.use(router);
 };
